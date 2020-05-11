9b797eefa6343d5e9fc7c870c4a0d03f1778f5ce
ZhengLuxi666 <2092028826@qq.com>
2020-05-11 23:35:54 +0800

    Update data.js

--- static/吴芷妍做作业/data.js
+++ static/吴芷妍做作业/data.js
@@ -1,12 +1,4 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-var endTime = new Date( "May 12, 2020 21:30:00" ); // 结束时间
-=======
-var endTime = new Date( "May 11, 2020 23:40:00" ); // 结束时间
->>>>>>> f5514c697cae559adff68e1c40228b75458208c9
-=======
 var endTime = new Date( "May 11, 2020 23:30:00" ); // 结束时间
->>>>>>> c85e897454ea3b7dc7434ada21012df9fcc4dd45
 var title = "吴芷妍, 快点做作业! 还剩: "; // 页面主旨
 var name = "吴芷妍做作业"; // cookie
 var password = "13579"; // cookie
