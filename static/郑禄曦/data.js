4b3e29119adc158b930fad0d35fdfa2b827e2536
ZhengLuxi666 <2092028826@qq.com>
2020-05-11 23:47:01 +0800

    s

--- static/郑禄曦/data.js
+++ static/郑禄曦/data.js
@@ -1,4 +1,4 @@
 var endTime = new Date( "May 12, 2020 00:00:00" ); // 结束时间
 var title = "郑禄曦, 请阅读! 还剩: "; // 页面主旨
-var name = "郑禄曦阅读"; // cookie
+var name = "郑禄曦"; // cookie
 var password = "1357924680"; // cookie
