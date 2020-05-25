50b68e2717d146e2158e2438c8f438f8bf671332
Zheng-Luxi <2092028826@qq.com>
2020-05-25 21:42:32 +0800

    Update data.js

--- static/吴芷妍/data.js
+++ static/吴芷妍/data.js
@@ -1,4 +1,4 @@
-var endTime = new Date( "May 24, 2020 20:30:00" ); // 结束时间
+var endTime = new Date( "May 25, 2020 22:20:00" ); // 结束时间
 var title = "吴芷妍, 快点做作业! 还剩: "; // 页面主旨
 var name = "吴芷妍"; // cookie
 var password = "13579"; // cookie
