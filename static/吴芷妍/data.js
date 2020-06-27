cc6fd3bfa2a6f0fb1756561e4315a88f1ec4e447
Zheng-Luxi <60222167+Zheng-Luxi@users.noreply.github.com>
2020-06-26 23:23:22 -0500

    Update data.js

--- static/吴芷妍/data.js
+++ static/吴芷妍/data.js
@@ -1,4 +1,4 @@
-var endTime = new Date( "June 22, 2020 21:30:00" ); // 结束时间
+var endTime = new Date( "June 27, 2020 21:30:00" ); // 结束时间
 var title = "吴芷妍, 吴姝萱, 快点做作业! 还剩: "; // 页面主旨
 var name = "吴芷妍"; // cookie
 var password = "13579"; // cookie
